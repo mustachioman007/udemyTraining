4206055e160b9b0ac05ab98f20b9bfa4e08848bb
mustachioman007 <rokitship402@yahoo.com>
2017-02-11 18:22:29 -0500

    scope

--- 04_scope/01_package-scope/02_visibility/main/main.go
+++ 04_scope/01_package-scope/02_visibility/main/main.go
@@ -1,3 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mustachioman007/udemyTraining/04_scope/01_package-scope/02_visibility/vis"
+)
 
+func main() {
+	fmt.Println(vis.MyName)
+	vis.PrintVar()
+}
