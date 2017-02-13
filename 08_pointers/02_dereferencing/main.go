11e14dd0a9dfcb5394e0230f3b11ed1af3e65e9d
mustachioman007 <rokitship402@yahoo.com>
2017-02-13 00:27:34 -0500

    memory address and pointers

---
+++ 08_pointers/02_dereferencing/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := 43
+
+	fmt.Println(a)  // 43
+	fmt.Println(&a) // 0xc04203e1c0
+
+	var b *int = &a
+
+	fmt.Println(b)  // 0xc04203e1c0
+	fmt.Println(*b) // 43
+
+	// b is an int pointer;
+	// b points to the memory address where an int is stored
+	// to see the value in that memory address, add a * in front of b
+	// this is known as dereferencing
+	// the * is an operator in this case
+}
